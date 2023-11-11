2b4055a1c30ed9198c5764b567dddf5c3d813a6b
HyunmoBae <tsi0520@naver.com>
2023-11-11 09:12:37 +0900

    feat(christmas) : add errorMessageType

--- src/main/java/christmas/error/ErrorMessage.java
+++ src/main/java/christmas/error/ErrorMessage.java
@@ -1,9 +1,24 @@
 package christmas.error;
 
 public class ErrorMessage {
+    public enum ErrorMessageType {
+        INVALID_DATE_VALUE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+        INVALID_NAME("[ERROR] 유효하지 않은 이름입니다. 다시 입력해 주세요."),
+        INVALID_MENU_VALUE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+        ;
 
-    enum ErrorMessageType {
+        private final String message;
 
+        ErrorMessageType(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
     }
-    
+
+    public static void printErrorMessage(ErrorMessageType errorMessageType) {
+        System.out.println(errorMessageType.message);
+    }
 }
