03de1916bc107824c1b1036d187ad8a2c910ae75
HyunmoBae <tsi0520@naver.com>
2023-11-13 08:24:26 +0900

    feat(christmas) : add constructor

--- src/main/java/christmas/data/reservation/ReservationInfo.java
+++ src/main/java/christmas/data/reservation/ReservationInfo.java
@@ -3,10 +3,22 @@ package christmas.data.reservation;
 import java.util.List;
 
 public class ReservationInfo {
-    private int totalAmountBeforeDiscount;
-    private int totalAmountAfterDiscount;
-    private int reservationDate;
-    private int totalDiscount;
-    private boolean gift;
-    private List<String> menu;
+    private static int totalAmountBeforeDiscount;
+    private static int totalAmountAfterDiscount;
+    private static int reservationDate;
+    private static int totalDiscount;
+    private static String gift;
+    private static List<String> menuOrders;
+
+    public void Reservation(int totalAmountBeforeDiscount, int totalAmountAfterDiscount, int reservationDate,
+                            int totalDiscount, String gift, List<String> menuOrders) {
+        this.totalAmountBeforeDiscount = totalAmountBeforeDiscount;
+        this.totalAmountAfterDiscount = totalAmountAfterDiscount;
+        this.reservationDate = reservationDate;
+        this.totalDiscount = totalDiscount;
+        this.gift = gift;
+        this.menuOrders = menuOrders;
+    }
+
+
 }
