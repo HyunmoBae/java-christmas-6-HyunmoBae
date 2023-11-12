d9b44d506cadaebe3b224254915d70c4bf1c4de6
HyunmoBae <tsi0520@naver.com>
2023-11-13 08:56:16 +0900

    feat(christmas) : add Reservation constructor

--- src/main/java/christmas/domain/Reservation.java
+++ src/main/java/christmas/domain/Reservation.java
@@ -1,11 +1,14 @@
 package christmas.domain;
 
+import christmas.data.reservation.ReservationInfo;
+
 public class Reservation {
 
-    public void makeReservation() {
+    public void Reservation(String name, Event event) {
+        ReservationInfo reservationInfo = new ReservationInfo(name, event.getTotalAmountBeforeDiscount(),
+                event.getTotalAmountAfterDiscount(event.getTotalAmountBeforeDiscount()), event.getDate(),
+                event.getTotalDiscount(), event.isGiftEvent(event.getTotalAmountBeforeDiscount()),
+                event.getMenuOrders());
     }
 
-    public void cancelReservation() {
-    }
-    
 }
