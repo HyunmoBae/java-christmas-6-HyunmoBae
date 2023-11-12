7f47453ce8b1f8711d39a95a78a587afb89981b0
HyunmoBae <tsi0520@naver.com>
2023-11-13 04:49:36 +0900

    feat(christmas) : add event functions

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -1,33 +1,159 @@
 package christmas.domain;
 
+import christmas.common.Menu.Appetizer;
+import christmas.common.Menu.Beverage;
+import christmas.common.Menu.Dessert;
+import christmas.common.Menu.MainDish;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
+    private String name = "";
+    private int date = 0;
+    private List<String> menuOrders = new ArrayList<>();
+    private int totalDiscount = 0;
 
-    public Event() {
+    Calendar calendar = new Calendar();
 
+    public Event(String name, int date, List<String> menuOrders) {
+        this.name = name;
+        this.date = date;
+        this.menuOrders = menuOrders;
+        christmasDdayDiscount();
+        weekDayDiscount();
+        weekendDiscount();
+        specialDiscount();
     }
 
-    public int christmasDdayDiscount() {
-        return 0;
+    private List<Integer> getAppetizerCountAndPrice() {
+        int totalCount = 0;
+        int totalPrize = 0;
+        for (String menuOrder : menuOrders) {
+            try {
+                Appetizer appetizer = Appetizer.valueOf(getOrderedMenuName(menuOrder));
+                totalPrize += appetizer.getPrice() * getOrderedMenuCount(menuOrder);
+                totalCount++;
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+        }
+        List<Integer> appetizerInfo = List.of(totalCount, totalPrize);
+        return appetizerInfo;
     }
 
-    public int weekDayDiscount() {
-        return 0;
+    private List<Integer> getMainDishCountAndPrice() {
+        int totalCount = 0;
+        int totalPrize = 0;
+        for (String menuOrder : menuOrders) {
+            try {
+                MainDish mainDish = MainDish.valueOf(getOrderedMenuName(menuOrder));
+                totalPrize += mainDish.getPrice() * getOrderedMenuCount(menuOrder);
+                totalCount++;
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+        }
+        List<Integer> mainDishInfo = List.of(totalCount, totalPrize);
+        return mainDishInfo;
     }
 
-    public int weekendDiscount() {
-        return 0;
+    private List<Integer> getDessertCountAndPrice() {
+        int totalCount = 0;
+        int totalPrize = 0;
+        for (String menuOrder : menuOrders) {
+            try {
+                Dessert dessert = Dessert.valueOf(getOrderedMenuName(menuOrder));
+                totalPrize += dessert.getPrice() * getOrderedMenuCount(menuOrder);
+                totalCount++;
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+        }
+        List<Integer> dessertInfo = List.of(totalCount, totalPrize);
+        return dessertInfo;
     }
 
-    public int specialDiscount() {
-        return 0;
+    private List<Integer> getBeverageCountAndPrice() {
+        int totalCount = 0;
+        int totalPrize = 0;
+        for (String menuOrder : menuOrders) {
+            try {
+                Beverage beverage = Beverage.valueOf(getOrderedMenuName(menuOrder));
+                totalPrize += beverage.getPrice() * getOrderedMenuCount(menuOrder);
+                totalCount++;
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+        }
+        List<Integer> beverageInfo = List.of(totalCount, totalPrize);
+        return beverageInfo;
     }
 
-    public boolean giftEvent() {
+    private void christmasDdayDiscount() {
+        if (date <= 25) {
+            totalDiscount += ((date - 1) * 100) + 1000;
+        }
+    }
+
+    private void weekDayDiscount() {
+        if (calendar.isDay(date).contains("weekday")) {
+            totalDiscount += getDessertCountAndPrice().get(0) * 2023;
+        }
+    }
+
+    private void weekendDiscount() {
+        if (calendar.isDay(date).contains("weekend")) {
+            totalDiscount += getMainDishCountAndPrice().get(0) * 2023;
+        }
+    }
+
+    private void specialDiscount() {
+        if (calendar.isDay(date).contains("specialDay")) {
+            totalDiscount += 1000;
+        }
+    }
+
+    public boolean giftEvent(int totalAmountBeforeDiscount) {
+        if (totalAmountBeforeDiscount >= 120000) {
+            return true;
+        }
         return false;
     }
 
     public String eventBadge() {
+        if (totalDiscount >= 20000) {
+            return "산타";
+        } else if (totalDiscount >= 10000) {
+            return "트리";
+        } else if (totalDiscount >= 5000) {
+            return "산타";
+        }
         return null;
+    }
+
+    private String getOrderedMenuName(String menuOrder) {
+        String menuName = menuOrder.substring(0, menuOrder.indexOf("-"));
+        return menuName;
+    }
+
+    private int getOrderedMenuCount(String menuOrder) {
+        int menuCount = menuOrder.indexOf("-" + 1);
+        return menuCount;
+    }
+
+    public int getTotalDiscount() {
+        return totalDiscount;
+    }
+
+    public int getTotalAmountAfterDiscount(int totalAmountBeforeDiscount) {
+        int totalAmountAfterDiscount = totalAmountBeforeDiscount - totalDiscount;
+        return totalAmountAfterDiscount;
+    }
+
+    public int getTotalAmountBeforeDiscount() {
+        int totalAmountBeforeDiscount = getAppetizerCountAndPrice().get(1) + getMainDishCountAndPrice().get(1)
+                + getDessertCountAndPrice().get(1) + getBeverageCountAndPrice().get(1);
+        return totalAmountBeforeDiscount;
     }
 
 }
