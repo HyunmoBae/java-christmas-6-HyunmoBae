3cdba9df678d05afeb83b7e1cc4a54a4ca7d2867
HyunmoBae <tsi0520@naver.com>
2023-11-14 07:00:19 +0900

    fix(christmas) : fix wrong test

--- src/test/java/christmas/domain/EventTest.java
+++ src/test/java/christmas/domain/EventTest.java
@@ -54,42 +54,42 @@ class EventTest {
     @Test
     void giftEventTest1() {
         event = new Event("abc", 15, List.of("타파스-1", "바비큐립-2", "해산물파스타-1", "제로콜라-1", "아이스크림-1"));
-        String result = event.isGiftEvent(120000);
+        String result = event.getGiftEvent(120000);
         assertThat(result).isEqualTo("샴페인 1개");
     }
 
     @Test
     void giftEventTest2() {
         event = new Event("abc", 15, List.of("타파스-1", "바비큐립-2", "해산물파스타-1", "제로콜라-1", "아이스크림-1"));
-        String result = event.isGiftEvent(10000);
+        String result = event.getGiftEvent(10000);
         assertThat(result).isEqualTo("없음");
     }
 
     @Test
     void getEventBadgeTest1() {
-        event = new Event("abc", 17, List.of("초코케이크-2", "바비큐립-1", "레드와인-1"));
-        String result = event.getEventBadge(event.getTotalDiscount());
-        assertThat(result).isEqualTo("별");
+        Event event = new Event("abc", 17, List.of("초코케이크-2", "바비큐립-1", "레드와인-1"));
+        String result = event.getEventBadge();
+        assertThat(result).isEqualTo("산타");
     }
 
     @Test
     void getEventBadgeTest2() {
-        event = new Event("abc", 17, List.of("초코케이크-5", "바비큐립-1", "레드와인-1"));
-        String result = event.getEventBadge(event.getTotalDiscount());
-        assertThat(result).isEqualTo("트리");
+        event = new Event("abc", 17, List.of("초코케이크-1", "바비큐립-1"));
+        String result = event.getEventBadge();
+        assertThat(result).isEqualTo("별");
     }
 
     @Test
     void getEventBadgeTest3() {
-        event = new Event("abc", 17, List.of("초코케이크-10", "바비큐립-1", "레드와인-1"));
-        String result = event.getEventBadge(event.getTotalDiscount());
-        assertThat(result).isEqualTo("산타");
+        event = new Event("abc", 17, List.of("초코케이크-1", "아이스크림-5", "바비큐립-1"));
+        String result = event.getEventBadge();
+        assertThat(result).isEqualTo("트리");
     }
 
     @Test
     void getEventBadgeTest4() {
         event = new Event("abc", 17, List.of("바비큐립-1", "레드와인-1"));
-        String result = event.getEventBadge(event.getTotalDiscount());
+        String result = event.getEventBadge();
         assertThat(result).isEqualTo("없음");
     }
 }
