ce91e12755b363c1e9623559b39d65a00b964037
HyunmoBae <tsi0520@naver.com>
2023-11-14 07:01:25 +0900

    feat(christmas) : add application

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,28 @@
 package christmas;
 
+import christmas.domain.Event;
+import christmas.ui.InputView;
+import christmas.ui.OutputView;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        outputView.printHello();
+        int date = Integer.valueOf(inputView.readDate());
+        outputView.printNotice();
+        List<String> menuOrders = inputView.readMenu();
+
+        Event event = new Event(date, menuOrders);
+        outputView.printMenu(event);
+        outputView.printTotalAmountBeforeDiscount(event);
+        outputView.printGiftMenu(event);
+        outputView.printEventHistory(event);
+        outputView.printTotalBenefitAmount(event);
+        outputView.printTotalAmountAfterDiscount(event);
+        outputView.printEventBadge(event);
     }
 }
