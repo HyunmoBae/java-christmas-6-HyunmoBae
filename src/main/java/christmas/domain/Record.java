d937daefa66295e09c39ae0e4d677342a05cb27f
HyunmoBae <tsi0520@naver.com>
2023-11-14 06:55:28 +0900

    refactor(christmas) : refactor Reservation, Record

--- src/main/java/christmas/domain/Record.java
+++ src/main/java/christmas/domain/Record.java
@@ -6,11 +6,13 @@ import christmas.data.person.PersonInfo;
 public class Record {
 
     public void createPersonInfo(String name, Event event) {
-        PersonInfo personInfo = new PersonInfo(name, event.getEventBadge(event.getTotalDiscount()));
+        PersonInfo personInfo = new PersonInfo(name, event.getEventBadge());
+        //이벤트 참여자 정보 저장하는 부분
     }
 
     public void createEventParticipationInfo(int totalPricipationPeople, long totalSales) {
         EventParticipationInfo eventParticipationInfo = new EventParticipationInfo(totalPricipationPeople, totalSales);
+        //이벤트 참여 정보 저장하는 부분
     }
 
 }
--- src/main/java/christmas/domain/Reservation.java
+++ src/main/java/christmas/domain/Reservation.java
@@ -7,8 +7,9 @@ public class Reservation {
     public void Reservation(String name, Event event) {
         ReservationInfo reservationInfo = new ReservationInfo(name, event.getTotalAmountBeforeDiscount(),
                 event.getTotalAmountAfterDiscount(event.getTotalAmountBeforeDiscount()), event.getDate(),
-                event.getTotalDiscount(), event.isGiftEvent(event.getTotalAmountBeforeDiscount()),
+                event.getTotalDiscount(), event.getGiftEvent(event.getTotalAmountBeforeDiscount()),
                 event.getMenuOrders());
+        //예약정보 저장하는 부분
     }
 
 }
